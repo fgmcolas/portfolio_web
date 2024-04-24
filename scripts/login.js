c54b899a28cfe0423202dc6a19b9486ee9b0c68e
fgmcolas <https://github.com/fgmcolas>
2024-04-25 00:38:12 +0200

    adding function to hide/show password onclick

---
+++ scripts/login.js
@@ -0,0 +1,17 @@
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+
+    iconEye.addEventListener('click', () => {
+        if (input.type === 'password') {
+            input.type = 'text'
+            iconEye.classList.add('ri-eye-line')
+            iconEye.classList.remove('ri-eye-off-line')
+        } else {
+            input.type = 'password'
+            iconEye.classList.remove('ri-eye-line')
+            iconEye.classList.add('ri-eye-off-line')
+        }
+    })
+}
+showHiddenPass('input-pass', 'input-icon')
