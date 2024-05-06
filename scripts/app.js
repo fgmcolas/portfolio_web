d28527fcb770c4dc301110075289ce41d864bcc6
fgmcolas <https://github.com/fgmcolas>
2024-05-06 05:41:32 +0200

    little z-index adjustment + indentation

--- scripts/app.js
+++ scripts/app.js
@@ -7,9 +7,9 @@ const app = express();
 const port = 3000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/myapp', { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+mongoose.connect('mongodb://localhost:27017/myapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 .then(() => {
     console.log('Connected to MongoDB');
