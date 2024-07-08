0f63e354ea6bcd5ed2c65210c26d5da65d4628bd
fgmcolas <https://github.com/fgmcolas>
2024-07-08 17:10:35 +0200

    Setting darkmode by default

--- Front/JS/theme.js
+++ Front/JS/theme.js
@@ -2,38 +2,30 @@ const body = document.querySelector('body');
 const btn = document.querySelector('.btn');
 const icon = document.querySelector('.btn__icon');
 
-//to save the dark mode use the object "local storage".
-
-//function that stores the value true if the dark mode is activated or false if it's not.
 function store(value) {
     localStorage.setItem('darkmode', value);
 }
 
-//function that indicates if the "darkmode" property exists. It loads the page as we had left it.
 function load() {
     const darkmode = localStorage.getItem('darkmode');
 
-    //if the dark mode was never activated
-    if (!darkmode) {
-        store(false);
-        icon.classList.add('fa-sun');
-    } else if (darkmode == 'true') { //if the dark mode is activated
+    if (darkmode === null) {
         body.classList.add('darkmode');
         icon.classList.add('fa-moon');
-    } else if (darkmode == 'false') { //if the dark mode exists but is disabled
+        store(true);
+    } else if (darkmode === 'true') {
+        body.classList.add('darkmode');
+        icon.classList.add('fa-moon');
+    } else {
         icon.classList.add('fa-sun');
     }
 }
 
-
 load();
 
 btn.addEventListener('click', () => {
-
     body.classList.toggle('darkmode');
     icon.classList.add('animated');
-
-    //save true or false
     store(body.classList.contains('darkmode'));
 
     if (body.classList.contains('darkmode')) {
@@ -46,5 +38,5 @@ btn.addEventListener('click', () => {
 
     setTimeout(() => {
         icon.classList.remove('animated');
-    }, 500)
-})
+    }, 500);
+});
